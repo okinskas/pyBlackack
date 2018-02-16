ae8fb913c89eb63767d06a0e0e7390b986f31261
O.Okinskas <oo302@kent.ac.uk>
2018-02-16 20:22:36 +0000

    Reducing to core functionality and adding test to calculate ace total.

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 import math
 import random
-
+#card implementation is fine
 class Card(object):
 
     ranks = [(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),('Jack',10),('Queen',10),('King',10),('Ace',11)]
@@ -21,6 +21,11 @@ class Card(object):
     def get_value(self):
         return self.rank[1]
 
+    def get_rank(self):
+        return self.rank
+
+
+# deck implementation is fine
 class Deck(object):
 
     def __init__(self):
@@ -46,7 +51,6 @@ class Hand(object):
     # for some reason cards=[] is not working, setting manually on creation while I figure it out
     def __init__(self, cards):
         self.cards = cards
-        self.stake = 0
         self.has_finished = False
         self.has_doubled_down = False
 
@@ -57,32 +61,21 @@ class Hand(object):
         return out
     
     def add_card(self, card):
-        # sort from smallest to largest with each addition, either add and sort, or check, insert, shift
-        for i in range(0, len(self.cards)):
-            if self.cards[i].get_value() > card.get_value():
-                self.cards.insert(i, card)
-                return
         self.cards.append(card)     
     
     def get_hand(self):
         return self.cards
 
     def split_hand(self):
-        hand1 = Hand([self.cards[0]])
-        hand1.stake = self.stake
-        hand2 = Hand([self.cards[1]])
-        hand2.stake = self.stake
-        return hand1, hand2
-
-    def double_down(self):
-        self.stake *= 2
-        self.has_doubled_down = True
+        if self.cards[0].get_key() == self.cards[1].get_key():
+            hand1 = Hand([self.cards[0]])
+            hand2 = Hand([self.cards[1]])
+            return hand1, hand2
+        else:
+            return None, None
 
     def clear_hand(self):
         self.cards = []
-
-    def can_split(self):
-        return self.cards[0].get_key() == self.cards[1].get_key()
 
     def is_blackjack(self):
         cards = self.cards
@@ -93,40 +86,44 @@ class Hand(object):
         else:
             return False
 
-    def calc_total(self, total=0, n=0): # likely still wrong
+    def calc_total(self, total=0, n=0, ace_count=0):
         if (n >= len(self.cards)):
             return total
-        
-        card_value = self.cards[n].get_value()
-        if card_value == 11 and total + card_value > 21:
-            card_value = 1
-        new_total = total + card_value
-        return self.calc_total(new_total, n + 1)
-
+        if self.cards[n].get_rank() == ('Ace', 11):
+            ace_count += 1
+        total += self.cards[n].get_value()
+        if total > 21 and ace_count > 0:
+            total -= 10
+            ace_count -= 1
+        return self.calc_total(total, n + 1, ace_count)
 
 class Player(object):
 
     def __init__(self, name):
         self.name = name
         self.hand_main = Hand([])
-        self.hand_split = None
+        self.hand_alt = None
 
     def __str__(self):
         out = self.name + ": "
         out += str(self.hand_main) + "\n"
-        out += str(self.hand_split)
+        out += str(self.hand_alt)
         return out + "\n"
 
     def split(self):
-        if self.hand_main.can_split():
-            self.hand_main, self.hand_split = self.hand_main.split_hand()
-            return True
-        else:
+        main, alt = self.hand_main.split()
+        if main == None:
             return False
+        else:
+            self.hand_main, self.hand_alt = main, alt
+            return True
 
     def reset_hands(self):
         self.hand_main = Hand([])
-        self.hand_split = None
+        self.hand_alt = None
+
+    def has_split(self):
+        return self.hand_alt != None
 
 class Dealer(Player):
 
@@ -146,8 +143,8 @@ class Dealer(Player):
         player_hand1 = player.hand_main.calc_total()
         player_hand2 = 0
 
-        if player.hand_split != None:
-            player_hand2 = player_hand2.calc_total()
+        if player.hand_alt != None:
+            player_hand2 = player.hand_alt.calc_total()
         if player_hand1 > 21 and player_hand2 > 21:
             return False
         else:
@@ -155,124 +152,66 @@ class Dealer(Player):
             is_soft_seventeen = dealer_hand.calc_total() == 17 and ('Ace',11) in dealer_hand.get_hand()
             return dealer_hand.calc_total() < 17 or is_soft_seventeen
 
+# standard game actions
 class Game(object):
 
     def __init__(self, name):
         self.player = Player(name)
         self.dealer = Dealer()
         self.deck = Deck()
+        self.turn_token = (0, 0)
+
+    def get_turn_hand(self):
+        if self.turn_token == (0, 0):
+            return self.dealer.hand_main
+        elif self.turn_token == (1, 0):
+            return self.player.hand_main
+        else:
+            return self.player.hand_alt
 
     def reset_deck(self):
         self.deck = Deck()
 
     def setup_deal(self):
-        
         self.dealer.hand_main.add_card(self.deck.get_card())
         self.dealer.hand_main.add_card(self.deck.get_card())
-        print(self.dealer.show_one())
         self.player.hand_main.add_card(self.deck.get_card())
         self.player.hand_main.add_card(self.deck.get_card())
 
-    def hit(self, hand):
+    def move_turn(self):
+        if self.turn_token == (0, 0):
+            self.turn_token = (1, 0)
+        elif self.turn_token == (1, 0) and  self.player.has_split():
+            self.turn_token = (1, 1)
+        else:
+            self.turn_token = (0,0)
+
+    def split(self):
+        if self.turn_token == (1, 0):
+            return self.player.split()
+        return False
+
+    def hit(self):
+        hand = self.get_turn_hand()
+        if hand.is_blackjack():
+            return False
+
         hand.add_card(self.deck.get_card())
 
-    def stick(self):
-        self.hand_position += 1
-        if self.hand_position < len(self.player.hands):
-            self.hand_position = 0
-            self.turn_position += 1
-
-    def split(self, player):
-        return player.split_hand(self.hand_position)
-
-    def player_cycle(self, hand):
-        while True:
-            # add check for blackjack prior to input
-            print(str(hand))
-            choice = input("What do you do?\n").lower()
-            if choice == "stick":
-                hand.has_finished = True
-                break
-            elif choice == "hit":
-                hand.add_card(self.deck.get_card())
-                if hand.calc_total() >= 21:
-                    print(str(hand))
-                    hand.has_finished = True
-                    break
-            elif choice == "split":
-                if not self.player.split():
-                    print("You cannot split this hand.\n")
-                else:
-                    break
-            elif choice == "double down" or choice == "dd":
-                hand.add_card(self.deck.get_card())
-                hand.has_doubled_down = True
-                hand.has_finished = True
-                break
-            elif choice == "quit":
-                SystemExit
-            else:
-                print("Not a valid choice.\n")
-
-    def dealer_cycle(self):
-        while True:
-            print(str(self.dealer))
-            if self.dealer.choice(self.player):
-                self.dealer.hand_main.add_card(self.deck.get_card())
-                continue
-            break
-    
-    def play(self):
-        self.player_cycle(self.player.hand_main)
-        if not self.player.hand_main.has_finished:
-            self.player_cycle(self.player.hand_main)
-
-        player_total1 = self.player.hand_main.calc_total()
-        is_blackjack1 = self.player.hand_main.is_blackjack()
-
-        player_total2 = 0
-        is_blackjack2 = False
-        
-        if self.player.hand_split != None:
-            self.player_cycle(self.player.hand_split)
-            player_total2 = self.player.hand_split.calc_total()
-            is_blackjack2 = self.player.hand_split.is_blackjack()
-        
-        if not ((is_blackjack1 or player_total1 > 21) and (is_blackjack2 or player_total2 > 21)):
-            self.dealer_cycle()
-
-        self.calc_winner()
-
-    def calc_winner(self): # need to check split hand and account for blackjack
-        dealer_total = self.dealer.hand_main.calc_total()
-        player_total1 = self.player.hand_main.calc_total()
-
-        if player_total1 > 21 or (player_total1 < dealer_total and dealer_total <= 21):
-            print("Dealer wins with: " + str(self.dealer.hand_main))
-        elif player_total1 > dealer_total or dealer_total > 21:
-            print(self.player.name + " wins with: " + str(self.player.hand_main))
-            pass
+        if hand.is_bust():
+            return False
         else:
-            print("Tied result")
-            print("Dealer: " + str(self.dealer.hand_main))
-            print(self.player.name + ": " + str(self.player.hand_main))
+            return True
 
 
-class Controller(object):
-    
-    def __init__(self, name):
-        game = Game(name)
-        game.setup_deal()
-        game.play()
+# w/o interface, sequence should be as follows:
 
-# add betting
-# add double down
-if __name__ == "__main__":
+# create game - initialise player name and stacks
+# deal hands (initialise deck)
+# player actions
+# dealer actions
+# calculate winner + distribute bets
+# deal new hands (initialise deck)
 
-    while True:
-        name = input("What is your name?\n")
-        if name == "Dealer":
-            print("Nice try, your name cannot be dealer. Please enter a different name.")
-            continue
-        break
-    Controller(name)
+# might make sense to use generators for getting cards from deck?
+
---
+++ testing.py
@@ -0,0 +1,23 @@
+import blackjack as bj
+
+def calc_test():
+    hand = bj.Hand([])
+
+    c1 = bj.Card((('Ace', 11), 'Clubs'))
+    c2 = bj.Card((('Ace', 11), 'Diamonds'))
+    c3 = bj.Card((('Ace', 11), 'Hearts'))
+    c4 = bj.Card((('Ace', 11), 'Spades'))
+
+    hand.add_card(c1)
+    hand.add_card(c2)
+    hand.add_card(c3)
+    hand.add_card(c4)
+
+    total = hand.calc_total()
+    print(total)
+
+    return total == 14
+         
+
+if __name__ == "__main__":
+    calc_test()
